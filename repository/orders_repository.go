46c637c66acd702f2608bd980c1627274f5daf90
iseT1enLoc <locnvt.it@gmail.com>
2024-12-13 21:47:15 +0700

    update order schema

--- repository/orders_repository.go
+++ repository/orders_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"cakewai/cakewai.com/domain"
 	"context"
+	"fmt"
 
 	"github.com/ydb-platform/ydb-go-sdk/v3/log"
 
@@ -44,10 +45,10 @@ func (o *orderRepository) CreateOrder(context context.Context, order domain.Orde
 	collection := o.db.Collection(o.collection_name)
 	_, err := collection.InsertOne(context, order)
 	if err != nil {
-		log.Error(err)
-		return nil, err
+		fmt.Printf("Failed to create order: %v", err)
+		return nil, fmt.Errorf("could not insert order: %w", err)
 	}
-	return &order, err
+	return &order, nil
 }
 
 // DONE
@@ -58,6 +59,8 @@ func (o *orderRepository) GetAllOrders(context context.Context) ([]*domain.Order
 		log.Error(err)
 		return nil, err
 	}
+	defer list_order_cursor.Close(context)
+
 	var list_order []*domain.Order
 	err = list_order_cursor.All(context, &list_order)
 	if err != nil {
@@ -70,23 +73,29 @@ func (o *orderRepository) GetAllOrders(context context.Context) ([]*domain.Order
 // DONE
 func (o *orderRepository) GetOrderByID(context context.Context, ID primitive.ObjectID) (*domain.Order, error) {
 	collection := o.db.Collection(o.collection_name)
-	var expected_order *domain.Order
+	var expected_order domain.Order
 	err := collection.FindOne(context, bson.M{"_id": ID}).Decode(&expected_order)
 	if err != nil {
-		log.Error(err)
-		return nil, err
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("order not found: %w", err)
+		}
+		fmt.Printf("Failed to fetch order by ID: %v", err)
+		return nil, fmt.Errorf("could not fetch order: %w", err)
 	}
-	return expected_order, nil
+	return &expected_order, nil
 }
 
 // DONE
 func (o *orderRepository) UpdateOrder(context context.Context, updatedOrder domain.Order) (*domain.Order, error) {
 	collection := o.db.Collection(o.collection_name)
 	updatedorder := bson.M{"$set": updatedOrder}
-	_, err := collection.UpdateOne(context, bson.M{"_id": updatedOrder.ID}, updatedorder)
+	res, err := collection.UpdateOne(context, bson.M{"_id": updatedOrder.ID}, updatedorder)
 	if err != nil {
-		log.Error(err)
-		return nil, err
+		fmt.Printf("Failed to update order: %v", err)
+		return nil, fmt.Errorf("could not update order: %w", err)
+	}
+	if res.MatchedCount == 0 {
+		return nil, fmt.Errorf("order not found")
 	}
 	return &updatedOrder, nil
 
