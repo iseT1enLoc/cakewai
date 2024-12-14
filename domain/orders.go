7b9ba90a92643dac1c7e7b2c5c9580268427c690
iseT1enLoc <locnvt.it@gmail.com>
2024-12-14 21:26:23 +0700

    update orders schema

--- domain/orders.go
+++ domain/orders.go
@@ -8,30 +8,35 @@ import (
 )
 
 type Payment struct {
-	PaymentMethod string `json:"payment_method" bson:"payment_method"`
-	IsPaid        int    `json:"is_paid" bson:"is_paid"`
+	PaymentMethod string `json:"payment_method" bson:"payment_method" default:"cash"`
+	IsPaid        int    `json:"is_paid" bson:"is_paid" default:"false"`
 }
 type Order struct {
 	ID              primitive.ObjectID `json:"id" bson:"_id"`
 	CustomerID      primitive.ObjectID `json:"customer_id" bson:"customer_id"`
+	CustomerName    string             `json:"customer_name" bson:"customer_name"`
+	ShippingAddress string             `json:"shipping_address" bson:"shipping_address"`
+	PhoneNumber     string             `json:"phone_number" bson:"phone_number"`
+	Notes           string             `json:"notes" bson:"notes"`
 	OrderItems      []CartItem         `json:"order_items" bson:"order_items"`
 	TotalPrice      float64            `json:"total_price" bson:"total_price"`
-	OrderStatus     string             `json:"order_status" bson:"order_status"  default:"pending"` // Can be "pending", "shipped", "delivered", "canceled"
-	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time          `json:"update_at" bson:"updated_at"`
-	ShippingAddress Address            `json:"shipping_address" bson:"shipping_address"`
-	PaymentInfo     Payment            `json:"payment_info" bson:"payment_info"`
-	ShippingStatus  string             `json:"shipping_status" bson:"shipping_status"`
+
+	PaymentInfo    Payment    `json:"payment_info" bson:"payment_info"`
+	OrderStatus    string     `json:"order_status" bson:"order_status"  default:"pending"` // Can be "pending", "shipped", "delivered", "canceled"
+	ShippingStatus string     `json:"shipping_status" bson:"shipping_status" default:"pending"`
+	CreatedAt      time.Time  `json:"created_at" bson:"created_at"`
+	UpdatedAt      *time.Time `json:"update_at" bson:"updated_at"`
 }
 type PaymentReq struct {
-	Order_id    string  `json:"order_id"`
+	Order_id    string  `json:"order_id" bson:"order_id"`
 	PaymentInfo Payment `json:"payment_info" bson:"payment_info"`
 }
 type OrderReq struct {
-	OrderItems     []CartItem `json:"items" bson:"items"`
-	ShippingAdress Address    `json:"shipping_address" bson:"shipping_address"`
-	PaymentInfo    Payment    `json:"payment_info" bson:"payment_info"`
-	ShippingStatus string     `json:"shipping_status" bson:"shipping_status"`
+	CustomerName    string     `json:"customer_name" bson:"customer_name"`
+	ShippingAddress string     `json:"shipping_address" bson:"shipping_address"`
+	PhoneNumber     string     `json:"phone_number" bson:"phone_number"`
+	Notes           string     `json:"notes" bson:"notes"`
+	OrderItems      []CartItem `json:"order_items" bson:"order_items"`
 }
 type OrderUsecase interface {
 	CreateOrder(context context.Context, order Order) (*Order, error)
